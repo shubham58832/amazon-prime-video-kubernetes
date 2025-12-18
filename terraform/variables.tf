8e1abc6b684c11372c07bed0c9c69b8c83ed68b9
Shubham Kumar Patidar <124715310+shubham58832@users.noreply.github.com>
2025-12-18 22:04:10 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,7 +3,7 @@ variable "instance_name" {
 }
 
 variable "key_name" {
-  default = "cloudaseem"                  # Names of key in aws
+  default = "primevideo"                  # Names of key in aws
 }
 
 
